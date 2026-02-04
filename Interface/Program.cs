e2e56e462b2dc5cb3ebb62c2461c50210f35edcb
Mohd Arshil Azim <md351769@gmail.com>
2026-02-04 14:57:20 +0530

    new question

---
+++ Interface/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InterfaceExample
+{
+    internal class Program
+    {
+        interface IExample
+        {
+            int DoSomething(int a)
+            {
+                return a * 2;
+            }
+        }
+
+        interface IAnotherExample
+        {
+            void DoSomethingElse();
+        }
+
+        class ExampleImplementation : IExample, IAnotherExample
+        {
+            public void DoSomethingElse()
+            {
+                Console.WriteLine("Doing something else!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Interface Example");
+
+            ExampleImplementation ex = new ExampleImplementation();
+            ex.DoSomethingElse();
+
+            // CALL DEFAULT INTERFACE METHOD USING INTERFACE REFERENCE
+            IExample ie = ex;
+            Console.WriteLine(ie.DoSomething(10));
+        }
+    }
+}
