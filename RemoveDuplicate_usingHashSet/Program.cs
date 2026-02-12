9f048df893ebee4d80c3883e993762745b5ceb4c
Mohd Arshil Azim <md351769@gmail.com>
2026-02-12 15:47:38 +0530

    new question

---
+++ RemoveDuplicate_usingHashSet/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RemoveDuplicate_usingHashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of elements in the array:"); int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("Enter the elements of the array:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            HashSet<int> uniqueElements = new HashSet<int>(arr); 
+            Console.WriteLine("Array after removing duplicates:");
+            foreach (int element in uniqueElements) {
+                
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
---
+++ ReverseStringWithinbuildFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ReverseStringWithinbuildFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to reverse:");
+            string input = Console.ReadLine();
+            // Using built-in function to reverse the string string reversed = new string(input.Reverse().ToArray()); Console.WriteLine($"Reversed string: {reversed}");
+            int n = input.Length;
+            char[] reverse = new char[n];
+            for(int i = 0; i < n; i++)
+            {
+                reverse[i] = input[n - 1 - i];
+            } 
+            string reversed = new string(reverse); 
+            Console.WriteLine($"Reversed string: {reversed}");
+        }
+    }
+}
