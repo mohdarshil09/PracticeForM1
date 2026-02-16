c30a045dc907dfc6f4fcf933cfc1551e0cb13f31
Mohd Arshil Azim <md351769@gmail.com>
2026-02-16 10:08:16 +0530

    ...

---
+++ Bank Statement – Spend by Category/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Bank_Statement___Spend_by_Category
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string category, double amount)> transactions = new List<(string, double)>
+            {
+                ("Groceries", -150),
+                ("Utilities", -80),
+                ("Entertainment", -120),
+                ("Groceries", -200),
+                ("Utilities", -60),
+                ("Entertainment", -90),
+                ("Salary", 3000)
+            };
+
+            //var spendByCategory = transactions
+            //    .Where(t => t.amount < 0)
+            //    .GroupBy(t => t.category)
+            //    .ToDictionary(
+            //    g => g.Key,
+            //        g => g.Sum(t => Math.Abs(t.amount))
+            //    );
+
+
+            var spendByCategory=transactions
+                .Where(p=>p.amount<0)
+                .GroupBy(p=>p.category)
+                .ToDictionary(
+                p=>p.Key,
+                p=>p.Sum(t=>Math.Abs(t.amount))
+                );
+
+            foreach (var spend in spendByCategory)
+            {
+                Console.WriteLine($" {spend.Key}: {spend.Value}");
+            }
+        }
+    }
+   
+}
---
+++ Inventory – Detect Duplicate Serials/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Inventory___Detect_Duplicate_Serials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> serials = new List<string>
+            {
+                "S1","S2","S1","S3","S2","S2"
+            };
+            HashSet<string> uniqueSerials = new HashSet<string>();
+            foreach (string serial in serials)
+            {
+                uniqueSerials.Add(serial);
+            }
+            foreach (string serial in uniqueSerials)
+            {
+                Console.WriteLine(string.Join(" ",serial));
+            }
+        }
+    }
+}
